57d7d162848a86ce06d58df650ea0b728de4d170
Sheroz Melikov <sherozmelikov043@gmail.com>
2026-05-26 10:39:17 +0100

    Add category schemas

--- app/schemas/categories.py
+++ app/schemas/categories.py
@@ -1,17 +1,17 @@
-from pydantic import BaseModel , ConfigDict , Field 
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class CategoryCreate(BaseModel):
-    name : str  = Field (min_length=1, max_length=50)
+    name: str = Field(min_length=1, max_length=50)
+
 
 class CategoryUpdate(BaseModel):
-    name : str | None = Field (default= None , min_length= 1, max_length= 50)
+    name: str | None = Field(default=None, min_length=1, max_length=50)
 
 
 class CategoryResponse(BaseModel):
-    id: int 
-    name : str
+    id: int
+    name: str
+    user_id: int
 
-    user_id : int 
-
-    model_config = ConfigDict (from_attributes=True)
+    model_config = ConfigDict(from_attributes=True)
--- app/schemas/tasks.py
+++ app/schemas/tasks.py
@@ -10,6 +10,7 @@ class TaskCreate(BaseModel):
     )
     description : str | None = Field(default=None, max_length=500)
     is_complete : bool = False
+    category_id : int | None = Field(default=None, gt=0)
 
 class TaskUpdate(BaseModel):
     title : str = Field(
@@ -18,7 +19,7 @@ class TaskUpdate(BaseModel):
     ) 
     description : str | None = Field(default=None, max_length=500)
     is_complete : bool = False
-
+    category_id: int | None = Field(default=None, gt=0)
 
 class TaskResponse(BaseModel):
     id : int
@@ -27,6 +28,6 @@ class TaskResponse(BaseModel):
     is_complete: bool
     created_at : datetime 
     user_id : int 
-
+    category_id: int | None = None
     model_config = ConfigDict(from_attributes=True)
 
