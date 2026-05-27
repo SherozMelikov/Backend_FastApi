eaf5096e2fc3af4342802c91697147dbd1248c52
Sheroz Melikov <sherozmelikov043@gmail.com>
2026-05-27 23:52:50 +0100

    Add category routes and service updates

--- app/services/category_service.py
+++ app/services/category_service.py
@@ -2,13 +2,14 @@ from sqlalchemy.orm import Session
 from app.db.models import Category
 from fastapi.exceptions import HTTPException
 
-from app.schemas.tasks import CategoryCreate, CategoryUpdate
+from app.schemas.categories import CategoryCreate, CategoryUpdate
 
-def get_categories_for_user(db: Session, user_id: int):
+
+def get_categories_for_user(db: Session, user_id):
     categories = db.query(Category).filter(Category.user_id == user_id).all()
     return categories
 
-def get_category_for_user(db: Session, user_id: int, category_id: int):
+def get_category_for_user(db: Session, user_id, category_id):
     category = db.query(Category).filter(
         Category.id == category_id,
         Category.user_id == user_id
@@ -17,7 +18,17 @@ def get_category_for_user(db: Session, user_id: int, category_id: int):
         raise HTTPException(status_code=404, detail="Category not found")
     return category
 
-def create_category(db: Session, user_id: int, category: CategoryCreate):
+def create_category(db: Session, user_id, category: CategoryCreate):
+    existing_category = db.query(Category).filter(
+        Category.user_id == user_id,
+        Category.name == category.name,
+
+    ).first()
+
+    if existing_category:
+        raise HTTPException(status_code=409, detail="Category already exists")
+    
+    
     new_category = Category(
         name=category.name,
         user_id=user_id
@@ -27,35 +38,38 @@ def create_category(db: Session, user_id: int, category: CategoryCreate):
     db.refresh(new_category)
     return new_category
 
-def update_category(db: Session, user_id: int, category_id: int, category: CategoryUpdate):
-    found_category = db.query(Category).filter(
-        Category.id == category_id,
-        Category.user_id == user_id
-    ).first()
+def update_category(db: Session, user_id, category_id, category: CategoryUpdate):
+    found_category = get_category_for_user(
+        db=db,
+        user_id=user_id,
+        category_id=category_id,
+    )
+    if category.name is not None:
+        existing_category = db.query(Category).filter(
+            Category.user_id == user_id,
+            Category.name == category.name,
+            Category.id != category_id
+        ).first()
 
-    if found_category is None:
-        raise HTTPException(status_code=404, detail="Category not found")
-
-    found_category.name = category.name
-    
+        if existing_category:
+            raise HTTPException(status_code=409, detail="Category already exists")
+        
+        found_category.name = category.name
 
     db.commit()
     db.refresh(found_category)
 
     return found_category
 
-def delete_category(db: Session, user_id: int, category_id: int):
-    found_category = db.query(Category).filter(
-        Category.id == category_id,
-        Category.user_id == user_id
-    ).first()
-
-    if not found_category:
-        raise HTTPException(status_code=404, detail="Category not found")
-
+def delete_category(db: Session, user_id, category_id):
+    found_category = get_category_for_user(
+        db=db,
+        user_id=user_id,
+        category_id=category_id,
+    )
+    
     db.delete(found_category)
     db.commit()
 
-    return {"message": "Category is successfully deleted!"}
 
     
